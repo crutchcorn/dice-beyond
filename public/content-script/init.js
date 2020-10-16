e345878d6db98e794ea1f601659390a8873d0fbd
Corbin Crutchley <crutchcorn@gmail.com>
2020-10-15 19:03:54 -0700

    Got dice coloring working from UI

--- public/content-script/init.js
+++ public/content-script/init.js
@@ -3,6 +3,10 @@ dataEl.name = "DiceBGHex";
 dataEl.content = `#000000`;
 document.body.appendChild(dataEl)
 
+chrome.storage.sync.get(['backgroundHex'], function(result) {
+    dataEl.content = result.backgroundHex;
+});
+
 const el = document.createElement('script')
 el.src = chrome.extension.getURL('web-accessible-resources/color-dice-xhr-intercept.js')
 document.body.appendChild(el)
