67e5827eec0f2f9c2173f89009e7f36538a2c05f
Corbin Crutchley <crutchcorn@gmail.com>
2020-10-22 17:46:26 -0700

    Added initial color picker styling

--- src/ColorPicker.js
+++ src/ColorPicker.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import {CustomPicker} from "react-color";
 import {Hue, Lightness, Saturation} from "./react-colors-extension/common";
+import './ColorPicker.css';
 
 export const ColorPicker = CustomPicker(props => {
     return (
         <>
-            <div style={{height: '1.25rem', margin: '0.25rem', position: 'relative'}}>
-            <Hue {...props} />
+            <div className="selectorContainer">
+                <p className="bodyText selectorText">H</p>
+                <div className="selectorBars">
+                    <Hue {...props} />
+                </div>
+            </div>
+            <div className="selectorContainer">
+                <p className="bodyText selectorText">S</p>
+                <div className="selectorBars">
+                    <Saturation {...props}/>
+                </div>
+            </div>
+            <div className="selectorContainer">
+                <p className="bodyText selectorText">L</p>
+                <div className="selectorBars">
+                    <Lightness {...props}/>
+                </div>
             </div>
 
-            <div style={{height: '1.25rem', margin: '0.25rem', position: 'relative'}}>
-
-                <Saturation {...props}/>
-            </div>
-            <div style={{height: '1.25rem', margin: '0.25rem', position: 'relative'}}>
-
-            <Lightness {...props}/>
-            </div>
         </>
     )
 });
